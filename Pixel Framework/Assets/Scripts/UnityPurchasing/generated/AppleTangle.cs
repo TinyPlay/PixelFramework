9c413d0fb872e3de4c55ee29ddb3afc7724817a8
TinyPlay <ceo@tpgames.ru>
2021-10-21 01:18:30 +0500

    IAP Manager Added
    Added IAP Manager based on Unity Purchasing

---
+++ Pixel Framework/Assets/Scripts/UnityPurchasing/generated/AppleTangle.cs
@@ -0,0 +1,18 @@
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    public class AppleTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("SECScaSwtiJMzKJQGxgAky4FQK/Q1bnTgdLo0+rl4Lbn5fDhtrDS8Obj4GHi7OPTYeLp4WHi4uMHckrq68jl4ubm5OHi9f2Ll5eTkNnMzJSZ02HildPt5eC2/uzi4hzn5+Dh4ir6kRa+7TacvHgRxuBZtmyuvu4S673TYeLy5eC2/sPnYeLr02Hi59OPhsOqjYDN0sXTx+Xgtufo8P6ik7pE5uqf9KO18v2XMFRowNikQDaM0/Ll4Lbn6fDpopOTj4bDqo2AzdLM02Ig5evI5eLm5uTh4dNiVfliUIGPhsOQl4KNh4KRh8OXhpGOkMOCbJBigyX4uOrMcVEbp6sTg9t99haXi4yRipea0vXT9+Xgtufg8O6ik0s/ncHWKcY2Ouw1iDdBx8DyFEJPz8OAhpGXioWKgIKXhsOTjI+KgJqH1sD2qPa6/lB3FBV/fSyzWSK7s8llq2UU7uLi5ubj04HS6NPq5eC2w4yFw5eLhsOXi4aNw4KTk4+KgIKXioWKgIKXhsOBmsOCjZrDk4KRl2Hi4+XqyWWrZRSAh+bi02IR08nlnKJLexoyKYV/x4jyM0BYB/jJIPyqO5V80PeGQpR3Ks7h4OLj4kBh4pGCgJeKgIbDkJeCl4aOho2XkM3T9dP35eC25+Dw7qKTk4+Gw7GMjJeUlM2Ck5OPhs2AjI7MgpOTj4aAgvxyOP2kswjmDr2aZ84I1UG0r7YPmsOCkJCWjoaQw4KAgIaTl4KNgIbsft4QyKrL+SsdLVZa7Tq9/zUo3mj6aj0aqI8W5EjB0+EL+90bs+owI4DQlBTZ5M+1CDnswu05WZD6rFZ2fZnvR6RouDf11NAoJ+yuLfeKMtbR0tfT0NW59O7Q1tPR09rR0tfT5+Xw4baw0vDT8uXgtufp8Omik5M61ZwiZLY6RHpa0aEYOzaSfZ1CsVT4XnChx/HJJOz+Va5/vYArqGP002HnWNNh4EBD4OHi4eHi4dPu5eqTj4bDoIaRl4qFioCCl4qMjcOill0XkHgNMYfsKJqs1ztB3RqbHIgr5dPs5eC2/vDi4hzn5tPg4uIc0/7F08fl4Lbn6PD+opOTj4bDoIaRl8OCjYfDgIaRl4qFioCCl4qMjcOTzaNFFKSunOu90/zl4Lb+wOf70/WNh8OAjI2HipeKjI2Qw4yFw5aQhoRs61fDFChPz8OMk1Xc4tNvVKAssYaPioKNgIbDjI3Dl4uKkMOAhpHVeq/Om1QOb3g/EJR4EZUxlNOsIsOgotNh4sHT7uXqyWWrZRTu4uLiVtlOF+zt43HoUsL1zZc23+44gfVS07sPuefRb4tQbP49hpAchL2GX97FhMNp0IkU7mEsPQhAzBqwibiHY/fIM4qkd5XqHReIbs2jRRSkrpymnfyviLN1omonl4Ho82CiZNBpYoqFioCCl4qMjcOilpeLjJGKl5rS5eC2/u3n9ef3yDOKpHeV6h0XiG6Tj4bDsYyMl8OgotP99O7T1dPX0eQPntpgaLDDMNsnUlx5rOmIHMgf7uXqyWWrZRTu4uLm5uPgYeLi47/HAQgyVJM87KYCxCkSjpsOBFb09PxmYGb4et6k1BFKeKNtzzdSc/E7s0lpNjkHHzPq5NRTlpbC");
+        private static int[] order = new int[] { 24,8,14,35,28,19,33,35,52,9,37,42,55,19,54,44,20,52,41,39,34,52,27,46,55,36,44,40,40,34,57,41,35,54,58,59,55,40,50,58,40,45,45,43,52,50,48,58,48,52,56,59,55,56,57,57,59,58,59,59,60 };
+        private static int key = 227;
+
+        public static readonly bool IsPopulated = true;
+
+        public static byte[] Data() {
+        	if (IsPopulated == false)
+        		return null;
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
